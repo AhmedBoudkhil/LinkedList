d2a38fa090e5f6b9de600a7728729ad9367d12ec
Vladimir_Syrano <90070683+ahmedhmida962@users.noreply.github.com>
2024-04-26 17:50:47 +0200

    Add files via upload
    C program to insert a node in specific position in  linked list .

---
+++ linkedList.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node{
+    int data;
+    struct node *link;
+}node;
+
+
+node *createNewNode(int data){
+    node *newNode = malloc(sizeof(node));
+    newNode->data  = data;
+    newNode->link  = NULL;
+    return newNode;
+}
+void addFirst(node **head, node *newNode){
+    newNode->link = *head;
+    *head = newNode;
+}
+
+node *AddNewNode(node *head,node *newNode,int pos){
+    if(pos == 1){
+        addFirst(&head,newNode);
+    }else{
+        node *tracker  = head;
+        pos -= 1;
+        while(pos != 1){
+            tracker = tracker->link;
+            pos--;
+            
+        }
+        newNode->link = tracker->link;
+        tracker->link = newNode;
+        
+    }
+    
+}
+void printLinkedList(node *first){
+    node *tmp = first;
+    
+    while(tmp != NULL){
+        printf("data : %d \n",tmp->data);
+        tmp = tmp->link;
+    }
+}
+int main() {
+    int pos = 1;
+    
+    node *first,*second,*last;
+    first = createNewNode(10);
+    second = createNewNode(20);
+    last = createNewNode(30);
+    
+    first->link = second;
+    second->link = last;
+    last ->link = NULL;
+    
+    node *newFuckinNode = createNewNode(999);
+    
+    if(pos == 1){
+        addFirst(&first,newFuckinNode);
+    }else{
+        AddNewNode(first,newFuckinNode,pos);
+    }
+    printLinkedList(first);
+    
+    
+    return 0;
+}
